f203feb1cbea0f591cfafe9f9be885887b18cad9
Han Sena <abisena@outlook.com>
2024-04-17 08:54:21 +0700

    Initial Commit

---
+++ EmojiSearch/EmojiSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiSearchApp.swift
+//  EmojiSearch
+//
+//  Created by Hidayat Abisena on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
